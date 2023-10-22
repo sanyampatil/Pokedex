b15a8f4b5f3341f8569b6d7d08d7cc3cbf8fcc61
sanyam <sanyampatil869@gmail.com>
2023-10-22 11:54:44 +0530

    first commit for project setup

---
+++ src/components/Pokedex/Pokedex.jsx
@@ -0,0 +1,13 @@
+import Search from "../Search/Search";
+import './pokedex.css'
+function Pokedex(){
+    return(
+        <div className="pokedex-wraper">
+        <h1>Pokedex</h1>
+
+        <Search/>
+        </div>
+
+    )
+}
+export default Pokedex;
